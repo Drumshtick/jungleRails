6dc089bdc635b12b139f28b023af2ca78b592af5
Drumshtick <ekodnar@outlook.com>
2022-01-06 14:10:01 -0600

    database queries

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,13 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["LOGINNAME"], password: ENV["PASSWRD"] 
 
   def show
+    @products_count = Product.all.count
+    @categories_count = Category.all.count
+    @categories = Category.all
+    @items_per_category = []
+    @categories.each do | category |
+      @items_per_category.push([category.name, Product.where(category_id: category.id).count])
+    end
+
   end
 end
