29c931eda4c352d9acd07750def2afdb2071c25e
Drumshtick <ekodnar@outlook.com>
2022-01-06 19:17:13 -0600

    create and destroy user session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # user exists, password exists
+    if user && user.authenticate(params[:password])
+      # Create session cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # On fail boot back to login
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    # On logout destroy session cookie
+    session[:user_id] = nil
+    # Boot 'em to login
+    redirect_to '/login'
   end
 end
