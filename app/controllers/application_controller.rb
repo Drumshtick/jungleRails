59b11ffed5d99884d8da97ddff3ef4eb76293964
Drumshtick <ekodnar@outlook.com>
2022-01-06 19:16:29 -0600

    added helper methods for user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
   private
 
   def cart
+    # a ||= b is a conditional assignment operator. It means:
+      # if a is undefined or falsey, then evaluate b and set a to the result.
+      # Otherwise (if a is defined and evaluates to truthy), then b is not evaluated, and no assignment takes place.
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
   helper_method :cart
@@ -27,6 +30,18 @@ class ApplicationController < ActionController::Base
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    # Add a before_filter to any controller that you want to secure.
+    # This will force user's to login
+    # 'before_filter :authorize'
+    redirect_to '/login' unless current_user
   end
 
 end
