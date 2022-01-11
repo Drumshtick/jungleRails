bd05275b16f291f9c4e82b710650f44010b1c759
Drumshtick <ekodnar@outlook.com>
2022-01-11 13:22:21 -0600

    paramater validations and min/max for password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: {in: 6..20}
+  validates :password_confirmation, presence: true
   # Gives the user model authentication methods from bcrypt
   has_secure_password
   
@@ -8,7 +13,15 @@ class User < ActiveRecord::Base
   
     # before saving before_save is called!
       # This will put the email to downcase before saving
-  before_save { self.email.downcase!}
+  before_save { validate_save}
+  def validate_save
+    if (self.first_name && self.last_name && self.email && self.email )
+      self.email.downcase!
+    else 
+      nil
+    end
+  end
+
 end
     # a ||= b is a conditional assignment operator. It means:
       # if a is undefined or falsey, then evaluate b and set a to the result.
