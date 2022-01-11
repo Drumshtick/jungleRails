b1e52aa7c4daa66c8aa280316afb776931a781a7
Drumshtick <ekodnar@outlook.com>
2022-01-11 16:19:55 -0600

    replaced .authenticate with authenticate_with_credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,23 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    # authenticate_with_credentials(params[:email], params[:password])
-    # user exists, password exists
-    if user && user.authenticate(params[:password])
-      # Create session cookie
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
-    # On fail boot back to login
       redirect_to '/login'
     end
   end
 
   def destroy
-    # On logout destroy session cookie
     session[:user_id] = nil
-    # Boot 'em to login
     redirect_to '/login'
   end
 end
