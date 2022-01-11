e57098c37c8e774adfb107a822cd69b72e53a78d
Drumshtick <ekodnar@outlook.com>
2022-01-11 16:20:47 -0600

    gits tatus

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,22 +7,17 @@ class User < ActiveRecord::Base
   # Gives the user model authentication methods from bcrypt
   has_secure_password
   
-  def authenticate_with_credentials(email, password)
-    @user = User.where(email: email, password: password)
+  def self.authenticate_with_credentials(email, password)
+    user = User.where(email: email.strip).first
+    if user
+      user.authenticate(password)
+    else
+      nil
+    end
   end
   
     # before saving before_save is called!
       # This will put the email to downcase before saving
-  before_save { validate_save}
-  def validate_save
-    if (self.first_name && self.last_name && self.email && self.email )
-      self.email.downcase!
-    else 
-      nil
-    end
-  end
+  before_save { self.email.downcase!}
 
 end
-    # a ||= b is a conditional assignment operator. It means:
-      # if a is undefined or falsey, then evaluate b and set a to the result.
-      # Otherwise (if a is defined and evaluates to truthy), then b is not evaluated, and no assignment takes place.
