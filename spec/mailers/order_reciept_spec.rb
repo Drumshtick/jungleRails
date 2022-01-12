e82de127b7c914643a02920f6a6a8592eb497b29
Drumshtick <ekodnar@outlook.com>
2022-01-12 17:36:09 -0600

    started rspec for order reciepts

--- spec/mailers/order_reciept_spec.rb
+++ spec/mailers/order_reciept_spec.rb
@@ -1,8 +1,16 @@
 require "rails_helper"
 
 RSpec.describe OrderReciept, type: :mailer do
+
+  before :each do
+    @order = Order.new
+      email: 'ekodnar@ex.com',
+      total_cents: 2600,
+      stripe_charge_id: 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+    )
+  end
   describe "thanks_for_shopping" do
-    let(:mail) { OrderReciept.thanks_for_shopping }
+    let(:mail) { OrderReciept.thanks_for_shopping(Order.last) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Thanks for shopping")
