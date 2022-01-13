f97b49b46b7e155e272cda88b5b0660f5ac3002a
Drumshtick <ekodnar@outlook.com>
2022-01-12 18:42:23 -0600

    handed off reviews for product page with user info

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,12 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @reviews = Review.where(product_id: params[:id])
+    @users = {}
+    @reviews.each do |item|
+      user = User.where(id: item.user_id).pluck(:first_name)
+      @users.store(item.user_id, user)
+    end
   end
 
 end
